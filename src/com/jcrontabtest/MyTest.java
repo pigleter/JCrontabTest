5705dea100a34eae1c29eda92e1ffd828ee530d9
pigleter <pigleter@gmail.com>
2017-01-10 17:40:50 +0800

    完成时间的计算

--- src/com/jcrontabtest/MyTest.java
+++ src/com/jcrontabtest/MyTest.java
@@ -1,18 +1,21 @@
 package com.jcrontabtest;
 
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Calendar;
 
 public class MyTest {
 	public static void main(String[] args) {
-		String[] inputCrontab = new String[6];
-		inputCrontab[0] = "1,3,11";
-		inputCrontab[1] = "*";
-		inputCrontab[2] = "1,6,14,31";
-		inputCrontab[3] = "*/7";
-		inputCrontab[4] = "3,5,8";
-		inputCrontab[5] = "2-8/2";
+		Calendar cl = Calendar.getInstance();
+		System.out.println(cl.getTime());
+		for(int i = 0; i < 8; i++){
+			getPlanList(cl, "2", "31", "*", "*", "*", "5");
+			System.out.println(cl.getTime());
+			cl.add(Calendar.SECOND, 1);
+		}
+		
+	}
+	
+	public static void getPlanList(Calendar cl, String ctM, String ctW, String ctD, String ctHH, String ctMM, String ctSS){
 
 		ArrayList<Integer> M = new ArrayList<Integer>();
 		ArrayList<Integer> W = new ArrayList<Integer>();
@@ -28,64 +31,66 @@ public class MyTest {
 		String[] vs_MM;
 		String[] vs_SS;
 		
-		int nowM = 0;
-		int nowW = 0;
-		int nowD = 0;
-		int nowHH = 0;
-		int nowMM = 0;
-		int nowSS = 0;
+		int newM = 0;
+		int newW = 0;
+		int newD = 0;
+		int newHH = 0;
+		int newMM = 0;
+		int newSS = 0;
+		
+		int oldY = 0;
+		int oldM = 0;
+		int oldD = 0;
+		int oldHH = 0;
+		int oldMM = 0;
+		
+		int repeatCount = 0;
 		
 		boolean isFoundDay = false;
+		boolean isFoundTime = false;
 		
 		int rangeFrom = 0;
 		int rangeTo = 0;
 		int rangePoint = 0;
 		int step = 0;
 		
-		Calendar cl = Calendar.getInstance();
-		cl.set(Calendar.MONTH, 0);
-		cl.set(Calendar.DAY_OF_MONTH, 31);
-		System.out.println(cl.getTime());
+		oldY = cl.get(Calendar.YEAR);
+		oldM = cl.get(Calendar.MONTH);
+		oldD = cl.get(Calendar.DATE);
+		oldHH = cl.get(Calendar.HOUR_OF_DAY);
+		oldMM = cl.get(Calendar.MINUTE);
 		
-		System.out.println(cl.get(Calendar.DAY_OF_MONTH));
-		System.out.println(cl.get(Calendar.DAY_OF_WEEK));
-		
-		cl.add(Calendar.DAY_OF_WEEK, 1);
-		
-		System.out.println(cl.get(Calendar.DAY_OF_MONTH));
-		System.out.println(cl.get(Calendar.DAY_OF_WEEK));
-		
-		if(inputCrontab[0].contains("*")){
+		if(ctM.contains("*")){
 			for(int i = 0; i < 12; i++){
 				M.add(i + 1);
 			}
 		}
 		else{
-			vs_M = inputCrontab[0].split(",");
+			vs_M = ctM.split(",");
 			for(int i = 0; i < vs_M.length; i++){
 				M.add(Integer.parseInt(vs_M[i]));
 			}
 		}
 		
-		if(inputCrontab[1].contains("*")){
+		if(ctW.contains("*")){
 			for(int i = 0; i < 7; i++){
 				W.add(i + 1);
 			}
 		}
 		else{
-			vs_W = inputCrontab[1].split(",");
+			vs_W = ctW.split(",");
 			for(int i = 0; i < vs_W.length; i++){
 				W.add(Integer.parseInt(vs_W[i]));
 			}
 		}
 		
-		if(inputCrontab[2].contains("*")){
+		if(ctD.contains("*")){
 			for(int i = 0; i < 31; i++){
 				D.add(i + 1);
 			}
 		}
 		else{
-			vs_D = inputCrontab[2].split(",");
+			vs_D = ctD.split(",");
 			for(int i = 0; i < vs_D.length; i++){
 				D.add(Integer.parseInt(vs_D[i]));
 			}
@@ -95,19 +100,19 @@ public class MyTest {
 		rangeTo = 0;
 		rangePoint = 0;
 		step = 0;
-		if(inputCrontab[3].contains("*")){
-			inputCrontab[3] = inputCrontab[3].replace("*", "0-23");
+		if(ctHH.contains("*")){
+			ctHH = ctHH.replace("*", "0-23");
 		}
-		if(inputCrontab[3].contains("-")){
-			if(inputCrontab[3].contains("/")){
-				rangeFrom = Integer.parseInt(inputCrontab[3].split("/")[0].split("-")[0]);
-				rangeTo = Integer.parseInt(inputCrontab[3].split("/")[0].split("-")[1]);
-				step = Integer.parseInt(inputCrontab[3].split("/")[1]);
+		if(ctHH.contains("-")){
+			if(ctHH.contains("/")){
+				rangeFrom = Integer.parseInt(ctHH.split("/")[0].split("-")[0]);
+				rangeTo = Integer.parseInt(ctHH.split("/")[0].split("-")[1]);
+				step = Integer.parseInt(ctHH.split("/")[1]);
 				rangePoint = rangeFrom;
 			}
 			else{
-				rangeFrom = Integer.parseInt(inputCrontab[3].split("-")[0]);
-				rangeTo = Integer.parseInt(inputCrontab[3].split("-")[1]);
+				rangeFrom = Integer.parseInt(ctHH.split("-")[0]);
+				rangeTo = Integer.parseInt(ctHH.split("-")[1]);
 				step = 1;
 				rangePoint = rangeFrom;
 			}
@@ -117,7 +122,7 @@ public class MyTest {
 			}
 		}
 		else{
-			vs_HH = inputCrontab[3].split(",");
+			vs_HH = ctHH.split(",");
 			for(int i = 0; i < vs_HH.length; i++){
 				HH.add(Integer.parseInt(vs_HH[i]));
 			}
@@ -127,19 +132,19 @@ public class MyTest {
 		rangeTo = 0;
 		rangePoint = 0;
 		step = 0;
-		if(inputCrontab[4].contains("*")){
-			inputCrontab[4] = inputCrontab[4].replace("*", "0-59");
+		if(ctMM.contains("*")){
+			ctMM = ctMM.replace("*", "0-59");
 		}
-		if(inputCrontab[4].contains("-")){
-			if(inputCrontab[4].contains("/")){
-				rangeFrom = Integer.parseInt(inputCrontab[4].split("/")[0].split("-")[0]);
-				rangeTo = Integer.parseInt(inputCrontab[4].split("/")[0].split("-")[1]);
-				step = Integer.parseInt(inputCrontab[4].split("/")[1]);
+		if(ctMM.contains("-")){
+			if(ctMM.contains("/")){
+				rangeFrom = Integer.parseInt(ctMM.split("/")[0].split("-")[0]);
+				rangeTo = Integer.parseInt(ctMM.split("/")[0].split("-")[1]);
+				step = Integer.parseInt(ctMM.split("/")[1]);
 				rangePoint = rangeFrom;
 			}
 			else{
-				rangeFrom = Integer.parseInt(inputCrontab[4].split("-")[0]);
-				rangeTo = Integer.parseInt(inputCrontab[4].split("-")[1]);
+				rangeFrom = Integer.parseInt(ctMM.split("-")[0]);
+				rangeTo = Integer.parseInt(ctMM.split("-")[1]);
 				step = 1;
 				rangePoint = rangeFrom;
 			}
@@ -149,7 +154,7 @@ public class MyTest {
 			}
 		}
 		else{
-			vs_MM = inputCrontab[4].split(",");
+			vs_MM = ctMM.split(",");
 			for(int i = 0; i < vs_MM.length; i++){
 				MM.add(Integer.parseInt(vs_MM[i]));
 			}
@@ -159,19 +164,19 @@ public class MyTest {
 		rangeTo = 0;
 		rangePoint = 0;
 		step = 0;
-		if(inputCrontab[5].contains("*")){
-			inputCrontab[5] = inputCrontab[5].replace("*", "0-59");
+		if(ctSS.contains("*")){
+			ctSS = ctSS.replace("*", "0-59");
 		}
-		if(inputCrontab[5].contains("-")){
-			if(inputCrontab[5].contains("/")){
-				rangeFrom = Integer.parseInt(inputCrontab[5].split("/")[0].split("-")[0]);
-				rangeTo = Integer.parseInt(inputCrontab[5].split("/")[0].split("-")[1]);
-				step = Integer.parseInt(inputCrontab[5].split("/")[1]);
+		if(ctSS.contains("-")){
+			if(ctSS.contains("/")){
+				rangeFrom = Integer.parseInt(ctSS.split("/")[0].split("-")[0]);
+				rangeTo = Integer.parseInt(ctSS.split("/")[0].split("-")[1]);
+				step = Integer.parseInt(ctSS.split("/")[1]);
 				rangePoint = rangeFrom;
 			}
 			else{
-				rangeFrom = Integer.parseInt(inputCrontab[5].split("-")[0]);
-				rangeTo = Integer.parseInt(inputCrontab[5].split("-")[1]);
+				rangeFrom = Integer.parseInt(ctSS.split("-")[0]);
+				rangeTo = Integer.parseInt(ctSS.split("-")[1]);
 				step = 1;
 				rangePoint = rangeFrom;
 			}
@@ -181,80 +186,133 @@ public class MyTest {
 			}
 		}
 		else{
-			vs_SS = inputCrontab[5].split(",");
+			vs_SS = ctSS.split(",");
 			for(int i = 0; i < vs_SS.length; i++){
 				SS.add(Integer.parseInt(vs_SS[i]));
-			}
-		}
-		
-		nowSS = cl.get(Calendar.SECOND);
-		for(int i = 0; i < SS.size(); i++){
-			if(nowSS < SS.get(i)){
-				cl.set(Calendar.SECOND, SS.get(i));
-				break;
-			}
-			else if(i == SS.size() - 1){
-				cl.set(Calendar.SECOND, SS.get(0));
-				cl.add(Calendar.MINUTE, 1);
-			}			
-		}
-		
-		nowMM = cl.get(Calendar.MINUTE);
-		for(int i = 0; i < MM.size(); i++){
-			if(nowMM < MM.get(i)){
-				cl.set(Calendar.MINUTE, MM.get(i));
-				break;
-			}
-			else if(i == MM.size() - 1){
-				cl.set(Calendar.MINUTE, MM.get(0));
-				cl.add(Calendar.HOUR_OF_DAY, 1);
-			}
-		}
-		
-		nowHH = cl.get(Calendar.HOUR_OF_DAY);
-		for(int i = 0; i < HH.size(); i++){
-			if(nowHH < HH.get(i)){
-				cl.set(Calendar.HOUR_OF_DAY, HH.get(i));
-				break;
-			}
-			else if(i == HH.size() - 1){
-				cl.set(Calendar.HOUR_OF_DAY, HH.get(0));
-				cl.add(Calendar.DATE, 1);
 			}
 		}		
 				
 		isFoundDay = false;
 		while(!isFoundDay){
-			nowD = cl.get(Calendar.DAY_OF_MONTH);
-			for(int i = 0; i < D.size(); i++){
-				if(nowD < D.get(i)){
-					cl.set(Calendar.DAY_OF_MONTH, D.get(i));
-					nowW = cl.get(Calendar.DAY_OF_WEEK);
-					if(nowW - 1 == 0){
-						nowW = 7;
+			newM = cl.get(Calendar.MONTH) + 1;
+			for(int i = 0; i < M.size(); i++){
+				if(newM <= M.get(i)){
+					cl.set(Calendar.MONTH, M.get(i) - 1);
+					if(newM == M.get(i)){
+						newD = cl.get(Calendar.DATE);
 					}
 					else{
-						nowW = nowW - 1;
+						newD = 1;
 					}
-					for(int j = 0; j < W.size(); j++){
-						if(nowW == W.get(j)){
-							isFoundDay = true;
+					cl.set(Calendar.DATE, newD);
+					for(int j = 0; j < D.size(); j++){
+						if(newD <= D.get(j)){
+							if(D.get(j) > getLastDay(cl)){
+								break;
+							}
+							cl.set(Calendar.DATE, D.get(j));
+							newW = cl.get(Calendar.DAY_OF_WEEK);
+							if(newW - 1 == 0){
+								newW = 7;
+							}
+							else{
+								newW = newW - 1;
+							}
+							for(int k = 0; k < W.size(); k++){
+								if(newW == W.get(k)){
+									isFoundDay = true;
+									break;
+								}
+							}
+						}
+						if(isFoundDay){
 							break;
 						}
 					}
-					if(isFoundDay){
-						break;
-					}
+				}
+				if(isFoundDay){
+					break;
 				}
 			}
-			if(!isFoundDay){
-				cl.set(Calendar.DAY_OF_MONTH, D.get(0));
-				cl.add(Calendar.MONTH, 1);
+			if(isFoundDay){
+				
+				if(oldY == cl.get(Calendar.YEAR) && oldM == cl.get(Calendar.MONTH) && oldD == cl.get(Calendar.DATE)){
+					
+				}
+				else{
+					cl.set(Calendar.HOUR_OF_DAY, 0);
+					cl.set(Calendar.MINUTE, 0);
+					cl.set(Calendar.SECOND, 0);
+				}		
+				
+				newHH = cl.get(Calendar.HOUR_OF_DAY);
+				for(int i = 0; i < HH.size(); i++){
+					if(newHH <= HH.get(i)){
+						cl.set(Calendar.HOUR_OF_DAY, HH.get(i));
+						if(cl.get(Calendar.HOUR_OF_DAY) != oldHH){
+							cl.set(Calendar.MINUTE, 0);
+							cl.set(Calendar.SECOND, 0);
+						}
+						
+						newMM = cl.get(Calendar.MINUTE);
+						for(int j = 0; j < MM.size(); j++){
+							if(newMM <= MM.get(j)){
+								cl.set(Calendar.MINUTE, MM.get(j));
+								if(cl.get(Calendar.MINUTE) != oldMM){
+									cl.set(Calendar.SECOND, 0);
+								}
+								
+								newSS = cl.get(Calendar.SECOND);
+								for(int k = 0; k < SS.size(); k++){
+									if(newSS <= SS.get(k)){
+										cl.set(Calendar.SECOND, SS.get(k));
+										isFoundTime = true;
+										break;
+									}		
+								}
+								if(isFoundTime){
+									break;
+								}
+							}
+						}
+						if(isFoundTime){
+							break;
+						}
+					}
+				}			
+				
 			}
-		}		
-		
-		nowM = cl.get(Calendar.MONTH) + 1;
-		
-		System.out.println(cl.getTime());
+			else{
+				cl.add(Calendar.YEAR, 1);
+				cl.set(Calendar.MONTH, 0);
+				cl.set(Calendar.DATE, 1);
+				repeatCount = repeatCount + 1;
+			}
+			
+			if(isFoundTime){
+				break;
+			}
+			else{
+				isFoundDay = false;
+				cl.add(Calendar.DATE, 1);
+				cl.set(Calendar.HOUR_OF_DAY, 0);
+				cl.set(Calendar.MINUTE, 0);
+				cl.set(Calendar.SECOND, 0);
+			}
+			
+			if(repeatCount > 100){
+				System.out.println("100年内无符合日期时间！");
+				break;
+			}
+		}	
+	}
+	
+	public static int getLastDay(Calendar cl){
+		int lastDay = 0;
+		cl.set(Calendar.DATE, 1);
+		cl.add(Calendar.MONTH, 1);
+		cl.add(Calendar.DATE, -1);
+		lastDay = cl.get(Calendar.DATE);
+		return lastDay;
 	}
 }
